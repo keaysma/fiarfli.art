98673989de2b83b5f08effb084f312994be65372
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2024-12-27 15:12:40 -0500

    Move and revise blockgrid

--- src/views/BlockGrid.vue
+++ src/views/BlockGrid.vue
@@ -1,34 +1,3 @@
-<template>
-  <div id="art" class="page">
-    <div class="art-content">
-      <!-- <h1>Art</h1> -->
-      <!-- <div class="tag-list">
-        <button v-for="(tag, tagIndex) in tags" :key="tagIndex" :id="`tag-${tag}`"
-          @click="selectedTags.includes(tag) ? selectedTags.splice(selectedTags.indexOf(tag), 1) : selectedTags.push(tag)">
-          {{ tag }} {{ selectedTags.includes(tag) ? "✓" : "" }}
-        </button>
-      </div> -->
-      <div v-for="(block, blockIndex) in filteredBlocks" :key="blockIndex" class="art-section" :id="`art-${block.name}`">
-        <template v-if="block.content.length > 0">
-          <h2 :id="block.name.toLowerCase().replaceAll(/\ /g, '-')">{{ block.name }}</h2>
-          <div class="art-container">
-            <template v-for="(content, contentIndex) in block.content" :key="contentIndex">
-              <template v-if="content.hidden !== true">
-                <Content :class="content.type" :style="{ gridArea: `span ${content.height} / span ${content.width}` }"
-                  :content="content" :displayIcon="true" :useColor="true" :useThumbnail="true"
-                  @click="gallerySettings = { blockIndex, contentIndex, open: true, fullscreen: false }" />
-              </template>
-            </template>
-          </div>
-        </template>
-      </div>
-    </div>
-  </div>
-
-  <!-- Gallery -->
-  <Gallery :blocks="filteredBlocks" :gallerySettings="gallerySettings" />
-</template>
-
 <script setup lang="ts">
 import { ref, onMounted, computed } from "vue";
 
@@ -103,21 +72,41 @@ onMounted(() => {
 });
 </script>
 
+<template>
+  <div id="art" class="page">
+    <!-- <h1>Art</h1> -->
+    <!-- <div class="tag-list">
+        <button v-for="(tag, tagIndex) in tags" :key="tagIndex" :id="`tag-${tag}`"
+          @click="selectedTags.includes(tag) ? selectedTags.splice(selectedTags.indexOf(tag), 1) : selectedTags.push(tag)">
+          {{ tag }} {{ selectedTags.includes(tag) ? "✓" : "" }}
+        </button>
+      </div> -->
+    <div v-for="(block, blockIndex) in filteredBlocks" :key="blockIndex" class="art-section" :id="`art-${block.name}`">
+      <template v-if="block.content.length > 0">
+        <h2 :id="block.name.toLowerCase().replaceAll(/\ /g, '-')">{{ block.name }}</h2>
+        <div class="art-container">
+          <template v-for="(content, contentIndex) in block.content" :key="contentIndex">
+            <template v-if="content.hidden !== true">
+              <Content :class="content.type" :style="{ gridArea: `span ${content.height} / span ${content.width}` }"
+                :content="content" :displayIcon="true" :useColor="true" :useThumbnail="true"
+                @click="gallerySettings = { blockIndex, contentIndex, open: true, fullscreen: false }" />
+            </template>
+          </template>
+        </div>
+      </template>
+    </div>
+  </div>
+
+  <!-- Gallery -->
+  <Gallery :blocks="filteredBlocks" :gallerySettings="gallerySettings" />
+</template>
+
 <style lang="scss">
 #art.page {
-  display: flex;
-  flex-direction: column;
-
-  width: 100%;
-  min-height: 100vh;
-
   margin: 0;
-
   overflow: hidden;
+  text-align: center;
 
-  h1, h2 {
-    text-align: center;
-  }
 
   .tag-list {
     display: flex;
@@ -145,26 +134,12 @@ onMounted(() => {
     }
   }
 
-  .art-content {
-    margin: 75px 125px;
-    top: 200px;
-
-    color: #534957;
-    text-shadow: 1px 1px 1px #d389a7f4;
-  }
-
   .art-section {
-    margin: 0 0 50px 0;
-
     max-width: 900px;
     margin: auto;
 
-    h2 {
-      position: sticky;
-      top: 0px;
-      z-index: 11;
-
-      padding: 5px 0px;
+    >h2 {
+      scroll-margin-top: 120px;
     }
   }
 
@@ -174,8 +149,6 @@ onMounted(() => {
     gap: 8px;
 
     &>.content {
-      //max-width: 20vw;
-      // max-height: 20vw;
       transition: filter 0.15s;
 
       &:hover:not(.iframe, .contained),
@@ -188,25 +161,8 @@ onMounted(() => {
 
 @media only screen and (max-width: 768px) {
   #art.page {
-    width: 100%;
-    //min-height: 300vh;
-
-    margin: 0;
-
-    .art-content {
-      margin: 50px 10vw;
-      //position: sticky;
-      //top: 100px;
-
-      //color: dimgrey;
-      text-shadow: 1px 1px 3px #0004;
-    }
-
     .art-section {
-      h2 {
-        font-size: 1.5rem;
-        position: initial;
-      }
+      // padding-top: 100px;
     }
 
     .art-container {
@@ -230,53 +186,6 @@ onMounted(() => {
 
     .gallery-right {
       left: 125px;
-    }
-
-    .art-content {
-      width: clamp(200px, 100vw, 100%);
-      height: 50vh;
-
-      margin: 100px 0;
-      padding: 0;
-
-      img {
-        width: 100%;
-        height: 100%;
-
-        margin: 0;
-        padding: 0;
-
-        object-fit: cover;
-      }
-
-
-
-
-      .content>* {
-
-        &:hover,
-        &:focus {
-          filter: brightness(0.95);
-        }
-      }
-
-      .gallery-text {
-        margin: 25px;
-
-        .gallery-header {
-          flex-direction: column;
-
-          h1 {
-            margin: 25px 0 0 0;
-          }
-
-          h2 {
-            margin: 0;
-
-            font-size: 0.5rem;
-          }
-        }
-      }
     }
   }
 }
