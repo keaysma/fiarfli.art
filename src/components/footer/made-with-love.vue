6906ead21cc9dcb7701f1da36cf0c6543923d545
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2024-12-27 13:42:43 -0500

    consolidate CSS

--- src/components/footer/made-with-love.vue
+++ src/components/footer/made-with-love.vue
@@ -1,43 +1,33 @@
-<template>
-    <footer>
-        <p class="text-content">
-            <span>Made with</span>
-            <FontAwesomeIcon class="footer-icon" :icon="faHeart"/>
-            <a href="https://keays.io" target="_blank">keays.io</a>
-        </p>
-    </footer>
-</template>
-
 <script setup lang="ts">
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 </script>
 
+<template>
+    <footer>
+        <p>
+            <span>Made with</span>
+            <FontAwesomeIcon class="footer-icon" :icon="faHeart" />
+            <a href="https://keays.io" target="_blank">keays.io</a>
+        </p>
+    </footer>
+</template>
+
 <style lang="scss">
 footer {
-    position: relative;
-    width: 100%;
-    height: 2.5em;
+    height: 64px;
+    padding: 1em 0 0;
 
-    margin: 0;
+    >p {
+        width: max-content;
+        margin: 0 auto;
+        white-space: nowrap;
 
-    > .text-content {
-        display: flex;
-        flex-direction: row;
-        align-content: center;
-        justify-content: center;
-
-        margin: 0px auto;
-
-        color: #534957;
-        text-shadow: 1px 1px 3px #53495744;
-
-        > .footer-icon {
-            width: 0.75em;
+        >.footer-icon {
             color: #B2DEED;
             filter: drop-shadow(0 0 3px #53495744);
 
-            margin: 0 0.25em;
+            margin: 0 0.5em;
         }
     }
 }
