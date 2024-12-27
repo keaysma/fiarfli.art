e0e752740a88d8fe4c7711126b6b8b50a46fb03b
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2024-12-27 15:15:49 -0500

    Fix admin view links

--- src/components/admin/View.vue
+++ src/components/admin/View.vue
@@ -2,18 +2,19 @@
     <div id="admin-view">
         <About v-if="currentPage === 'about'" :about="content.about" />
         <Art v-if="currentPage === 'gallery'" :blocks="blocks" />
-        <Commissions v-if="currentPage === 'commissions'" :commissions="content.commissions" :commissionsEnabled="content.contactFormEnabled" />
+        <Commissions v-if="currentPage === 'commissions'" :commissions="content.commissions"
+            :commissionsEnabled="content.contactFormEnabled" />
         <Contact v-if="currentPage === 'contactme'" :contact="content.contact"
             :contactFormEnabled="content.contactFormEnabled" />
     </div>
 </template>
 
 <script setup lang="ts">
-import About from '../about/BasicAlt.vue'
-import Art from '../art/BlockGrid.vue'
-import Commissions from '../commissions/Commissions.vue'
-import Contact from '../../views/ContactMe.vue'
-import { AdminPage, Block, SiteContent } from '/src/types';
+import About from '/src/components/about/BasicAlt.vue'
+import Art from '/src/views/BlockGrid.vue'
+import Commissions from '/src/views/Commissions.vue'
+import Contact from '/src/views/ContactMe.vue'
+import type { AdminPage, Block, SiteContent } from '/src/types';
 
 defineProps<{
     currentPage: AdminPage;
